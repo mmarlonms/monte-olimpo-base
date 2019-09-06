2b0a18eb6371a22b465b92a16f3c37408506ee87
Marlon Miranda <marlon_kkkk@hotmail.com>
2019-09-06 17:10:21 -0300

    Ajuste de Projeto

--- src/MonteOlimpo.Domain/Repository/IDeusRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using MonteOlimpo.Base.Core.Domain.Repository;
-using MonteOlimpo.Domain.Models;
-
-namespace MonteOlimpo.Domain.Repository
-{
-    public interface IDeusRepository : IRepository<Deus>
-    {
-    }
-}
