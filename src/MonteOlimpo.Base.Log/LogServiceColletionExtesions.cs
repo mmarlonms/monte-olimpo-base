9e658fed0ee356b6874b839215074a22459c7577
Marlon Miranda <marlon_kkkk@hotmail.com>
2019-09-06 17:10:42 -0300

    Arquivos ignorados

---
+++ src/MonteOlimpo.Base.Log/LogServiceColletionExtesions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class LogServiceColletionExtesions
+    {
+        public static IServiceCollection AddMonteOlimpoLogging(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddLogging(c =>
+            {
+                c.ClearProviders();
+
+                Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                    .CreateLogger();
+
+                c.AddSerilog();
+            });
+
+            return services;
+        }
+    }
+}
