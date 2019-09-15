ff9d5b72680ba5fb531997a893a4d57759e85abb
Marlon Miranda <marlon_kkkk@hotmail.com>
2019-09-15 17:28:43 -0300

    Inclusão das classes de validações

---
+++ src/MonteOlimpo.Base.CoreException/ModelValidationError.cs
@@ -0,0 +1,9 @@
+﻿namespace MonteOlimpo.Base.CoreException
+{
+    public class ModelValidationError : CoreError
+    {
+        public ModelValidationError(string key, string message) : base(key, message)
+        {
+        }
+    }
+}
---
+++ src/MonteOlimpo.Base.CoreException/ModelValidationException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MonteOlimpo.Base.CoreException
+{
+    [Serializable]
+    public class ModelValidationException : CoreException<ModelValidationError>
+    {
+        public ModelValidationException()
+        {
+
+        }
+
+        public ModelValidationException(params ModelValidationError[] errors)
+        {
+            AddError(errors);
+        }
+
+        protected ModelValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public override string Key => "ModelValidationException";
+    }
+}
